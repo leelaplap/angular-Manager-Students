72dae8cba8b5de4e3098497922e3a71deed97d23
leelaplap <letandat27101998@gmail.com>
2019-11-26 15:48:21 +0700

    finished create

--- src/app/students/student-create/student-create.component.ts
+++ src/app/students/student-create/student-create.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
+import {StudentsService} from '../students.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-student-create',
@@ -6,10 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-create.component.css']
 })
 export class StudentCreateComponent implements OnInit {
+  submitted = false;
+  addStudentForm = this.fb.group({
+    name: ['', [Validators.required]],
+    age: ['', [Validators.required]],
+    address: ['', [Validators.required]],
+    star: ['', [Validators.required]],
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder,
+              private sv: StudentsService,
+              private router: Router
+  ) {
+  }
 
   ngOnInit() {
   }
 
+  submit() {
+    this.submitted = true;
+    const data = this.addStudentForm.value;
+    this.sv.add(data);
+    this.router.navigate(['/students']);
+  }
+
+  get name() {
+    return this.addStudentForm.get('name');
+  }
+
+  get age() {
+    return this.addStudentForm.get('age');
+  }
+
+  get address() {
+    return this.addStudentForm.get('address');
+  }
+
+  get star() {
+    return this.addStudentForm.get('star');
+  }
 }
